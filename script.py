88c10410095f743e08e23439f49837ec0670913b
Geoffrey Logovi <52314615+geoffreylgv@users.noreply.github.com>
2020-12-17 23:17:23 +0000

    Update script.py

--- script.py
+++ script.py
@@ -21,3 +21,17 @@ def factorial(n):
          return n*factorial(n-1)
     else:
         return 1
+
+"""
+is_palindrome ===> return true if is palindrome , else false
+"""
+def is_palindrome(word):
+    # FIXME
+    return word == word[::-1]
+    result = is_palindrome(word)
+ 
+    if result:
+        print("True")
+    else:
+        print("False")
+    
